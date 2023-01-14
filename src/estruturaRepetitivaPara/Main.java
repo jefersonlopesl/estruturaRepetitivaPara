d06f069cf0d3218354da8da81db7937f7ba1a10b
Jeferson Lopes <jefersonlopesluis@protonmail.com>
2023-01-14 11:10:42 -0300

    Initial commit

---
+++ src/estruturaRepetitivaPara/Main.java
@@ -0,0 +1,17 @@
+package estruturaRepetitivaPara;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Valor de i: " +i);
+			
+			
+		}
+	
+	
+	}
+
+}
